c24d3e4d3bde3df940afd1ad956a6a575ae040c4
utnso <martirolon.mr@gmail.com>
2020-04-10 21:11:13 -0300

    agrego el cambio del tp0.c

--- tp0.c
+++ tp0.c
@@ -1,78 +1,73 @@
-/*
+*
  * main.c
  *
  *  Created on: 28 feb. 2019
  *      Author: utnso
- */
+ /
 
 #include "tp0.h"
 
 int main(void)
 {
-	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
-	int conexion;
-	char* ip = "127.0.0.1";
-	char* puerto = "4444";
+    /---------------------------------------------------PARTE 2-------------------------------------------------------------/
+    int conexion;
 
-	t_log* logger;
-	t_config* config;
 
-	logger = iniciar_logger();
-	//Loggear "soy un log"
-	log_info(logger, "soy un log");
+    t_log logger;
+    t_config* config;
 
-	config = leer_config();
-	config_set_value(config, "IP", "127.0.0.1");
-	config_set_value(config, "PUERTO", "4444");
-	config_save(config);
+    logger = iniciar_logger();
+    //Loggear "soy un log"
+    log_info(logger, "soy un log");
 
-	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
+    config = leer_config();
+    char* ip = config_get_string_value(config, "IP");
+    char* puerto = config_get_string_value(config, "PUERTO");
 
-	//antes de continuar, tenemos que asegurarnos que el servidor esté corriendo porque lo necesitaremos para lo que sigue.
 
-	printf("crear conexion");
-	//crear conexion
-	conexion = crear_conexion(ip, puerto);
+    /---------------------------------------------------PARTE 3-------------------------------------------------------------/
 
-	printf("enviar mensaje");
+    //antes de continuar, tenemos que asegurarnos que el servidor esté corriendo porque lo necesitaremos para lo que sigue.
 
-	//enviar mensaje
-	char* mnsj = "holaa";
 
-	enviar_mensaje(mnsj, conexion);
+    //crear conexion
+    conexion = crear_conexion(ip, puerto);
 
-	printf("mensaje enviado");
+    //enviar mensaje
+    char* mnsj = "holaa";
 
-	//recibir mensaje
-	char* mensaje_recibido = recibir_mensaje(conexion);
+    enviar_mensaje(mnsj, conexion);
 
-	//loguear mensaje recibido
-	log_info(logger, mensaje_recibido);
+    //recibir mensaje
+    char* mensaje_recibido = recibir_mensaje(conexion);
 
-	terminar_programa(conexion, logger, config);
+    //loguear mensaje recibido
+    log_info(logger, mensaje_recibido);
 
-	return 0;
+    terminar_programa(conexion, logger, config);
+
+    return 0;
 
 }
 
 //TODO
 t_log* iniciar_logger(void)
 {
-	return log_create("/home/utnso/Game-watch-client/tp0.log", "tp0", 1, LOG_LEVEL_INFO);
+    return log_create("tp0.log", "tp0", 1, LOG_LEVEL_INFO);
 }
 
 //TODO
 t_config* leer_config(void)
 {
-	return config_create("/home/utnso/Game-watch-client/tp0.config");
+    return config_create("tp0.config");
 }
 
 //TODO
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
-	//Y por ultimo, para cerrar, hay que liberar lo que utilizamos (conexion, log y config) con las funciones de las commons y del TP mencionadas en el enunciado
+    //Y por ultimo, para cerrar, hay que liberar lo que utilizamos (conexion, log y config) con las funciones de las commons y del TP mencionadas en el enunciado
 
-	log_destroy(logger);
-	config_destroy(config);
-	liberar_conexion(conexion);
+    log_destroy(logger);
+    config_destroy(config);
+    liberar_conexion(conexion);
 }
